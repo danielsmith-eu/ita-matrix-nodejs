114e28690be90a0c5f08ca9f3e1db83a23980df7
Dr. Daniel A Smith <daniel@danielsmith.eu>
2016-04-25 20:05:21 +0100

    Progress

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var Flights = require('./flights');
 var Promise = require('promise');
 var moment = require('moment');
 var fs = require('fs');
+var clone = require('clone');
 
 var args = process.argv.slice(2);
 if (args.length != 1) {
@@ -13,97 +14,74 @@ if (args.length != 1) {
 var config = JSON.parse(fs.readFileSync(args[0], 'utf8'));
 var f = Flights();
 
-// get month by month search list
-var getMonthList = function() {
-    var dates = [];
-    var nextMonth = moment().month() + 2; // next month, and fix zero indexing
-    if (nextMonth <= 12) {
-        for (var month = nextMonth; month <= 12; ++month) {
-            // this year
-            dates.push({month: month, year: moment().isoWeekYear()});
-        }
-    }
-    // up to 11 months from now
-    for (var month = 1; month <= moment().month(); ++month) {
-        // next year
-        dates.push({month: month, year: moment().isoWeekYear() + 1});
-    }
-    return dates;
-}
+var getDates = function() {
+    return clone(config.dates);
+};
 
-var singleSearch = function(search) {
-    //console.log(search);
-    var dates = getMonthList();
-    return new Promise(function (resolve1, reject1) {
-        var doSearch = function(date) {
-            //console.log(date);
-            return new Promise(function (resolve2, reject2) {
-                f.search({
-                    fromAirports: config.from,
-                    toAirports: search.to,
-                    fromDays: 12,
-                    toDays: 14,
-                    year: date.year,
-                    month: date.month,
-                }).then(function(response) {
-                    try {
-                        var options = response.searchOptions;
-                        //console.log("results: ", response.results);
-                        var cheapestResult = f.cheapest(response.results);
-                        if (cheapestResult !== null) {
-                            //console.log("cheapestResult: ", cheapestResult);
-                            if (cheapestResult.priceInt <= search.threshold || config.showAll) {
-                                var formatted = options.fromAirports.join(",") +
-                                        "-" +
-                                        options.toAirports.join(",") +
-                                        " in " + options.year + "/" + options.month + ": " +
-                                        f.format(cheapestResult);
+var nextSearch = function() {
+    var dates = getDates();
+
+    var singleSearch = function(search) {
+        return new Promise(function (resolve1, reject1) {
+            var doSearch = function(date) {
+                return new Promise(function (resolve2, reject2) {
+                    var searchOptions = {
+                        fromAirports: config.from,
+                        toAirports: search.to,
+                        fromDays: config.duration.from,
+                        toDays: config.duration.to,
+                        year: date.year,
+                        month: date.month,
+                    };
+                    f.search(searchOptions).then(function(response) {
+                        try {
+                            var cheapestResult = f.cheapest(response.results);
+                            if (cheapestResult !== null && cheapestResult.priceInt <= search.threshold || config.showAll) {
+                                var formatted = searchOptions.fromAirports.join(",") + "-" +
+                                        searchOptions.toAirports.join(",") + " in " + searchOptions.year + "/" +
+                                        searchOptions.month + ": " + f.format(cheapestResult);
                                 return resolve2(formatted);
                             }
+                            return resolve2(null);
+                        } catch (e) {
+                            return reject2(e);
                         }
-                        return resolve2(null);
-                    } catch (e) {
+                    }, function (error) {
                         return reject2(e);
-                    }
-                }, function (error) {
-                    return reject2(e);
+                    });
                 });
-            });
-        }
-
-        var err = function (e) {
-            reject1(e);
-        };
-
-        var execSearch = function () {
-            if (dates.length > 0) {
-                doSearch(dates.shift()).then(dealWithResult, err);
-            } else {
-                resolve1();
             }
-        };
 
-        var dealWithResult = function (result) {
-            if (result !== null) {
-                // under threshold, print the flight
-                console.log(result);
-            }
+            var execSearch = function () {
+                if (dates.length > 0) {
+                    doSearch(dates.shift()).then(dealWithResult, reject1);
+                } else {
+                    resolve1();
+                }
+            };
+
+            var dealWithResult = function (result) {
+                if (result !== null) {
+                    // under threshold, print the flight
+                    console.log(result);
+                }
+                execSearch();
+            };
+
+            // start the first search
             execSearch();
-        };
-
-        // start the first search
-        execSearch();
-    });
-}
-
-var execSingleSearch = function () {
-    if (config.searches.length > 0) {
-        singleSearch(config.searches.shift()).then(execSingleSearch, function (e) {
-            console.log("Error: ", e);
         });
-    } else {
-        //console.log("Finished cleanly.");
     }
-}
 
-execSingleSearch();
+    var execSingleSearch = function () {
+        if (config.searches.length > 0) {
+            singleSearch(config.searches.shift()).then(execSingleSearch, function (e) {
+                console.log("Error: ", e);
+            });
+        } else {
+            //console.log("Finished cleanly.");
+        }
+    }
+
+    execSingleSearch();
+}

4f4e10e0c45ce666d197a4b4e385956cbf46083c
Dr Dan Smith <dan.smith@thomsonreuters.com>
2016-04-26 14:09:34 +0100

    Handle result parsing errors without completely bombing out

--- flights.js
+++ flights.js
@@ -107,29 +107,45 @@ module.exports = function(config) {
                         var results = [];
                         var allResults = body.result[7][1];
                         allResults.forEach(function (month) {
-                            // this is each month (i.e. each physical grid), usually 1 or 2
-                            var rows = month[1];
-                            rows.forEach(function (row) {
-                                // each row in the grid
-                                var days = row[1];
-                                days.forEach(function (day) {
-                                    var result = day[3];
-                                    if (result) {
-                                        var flights = result[1];
-                                        flights.forEach(function (flight) {
-                                            // cheapest flight per day, one
-                                            // per duration, e.g. 12, 13, 14 days length
-                                            results.push({
-                                                price: flight[2],
-                                                priceInt: parseInt(flight[2].substr(3)),
-                                                durationDays: flight[4],
-                                                outDate: flight[1][3][0][1],
-                                                inDate: flight[1][3][1][1],
-                                            });
+                            try {
+                                // this is each month (i.e. each physical grid), usually 1 or 2
+                                var rows = month[1];
+                                rows.forEach(function (row) {
+                                    try {
+                                        // each row in the grid
+                                        var days = row[1];
+                                        days.forEach(function (day) {
+                                            try {
+                                                var result = day[3];
+                                                if (result) {
+                                                    var flights = result[1];
+                                                    flights.forEach(function (flight) {
+                                                        try {
+                                                            // cheapest flight per day, one
+                                                            // per duration, e.g. 12, 13, 14 days length
+                                                            results.push({
+                                                                price: flight[2],
+                                                                priceInt: parseInt(flight[2].substr(3)),
+                                                                durationDays: flight[4],
+                                                                outDate: flight[1][3][0][1],
+                                                                inDate: flight[1][3][1][1],
+                                                            });
+                                                        } catch (e) {
+                                                            // do nothing
+                                                        }
+                                                    });
+                                                }
+                                            } catch (e) {
+                                                // no nothing
+                                            }
                                         });
+                                    } catch (e) {
+                                        // do nothing
                                     }
                                 });
-                            });
+                            } catch (e) {
+                                // do nothing
+                            }
                         });
                         return resolve({results: results});
                     } catch (e) {
